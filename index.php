fb1f62560d0d551a80be8a7141605e01913a17eb
sameersheikh2787 <developer.sameersheikh@gmail.com>
2026-02-01 03:03:47 +0530

    added

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-<<<<<<< HEAD
+
 $sum=0;
 $a=1;
 $b=5;
@@ -7,11 +7,10 @@ $b=5;
 $sum = $a + $b;
 
 echo $sum;
-=======
+
 echo "This is a tesing file rwarretest r fdsf" ;
 
 echo "index file is woking ifne";
 
 $tset ="HEllo";
->>>>>>> main
 ?>
