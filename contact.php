a5d4b66c15d048afdafecc67232ace9840dfe76c
sameersheikh2787 <developer.sameersheikh@gmail.com>
2026-02-01 15:28:29 +0530

    updatd

--- contact.php
+++
@@ -1,7 +0,0 @@
-<?php 
-<<<<<<< HEAD
-echo "This is a contact page f fdf fdsf";
-=======
-echo "This is a contact page";
->>>>>>> main
-?>
--- index.php
+++ index.php
@@ -4,4 +4,14 @@ echo "This is a tesing file rwarretest r fdsf" ;
 echo "index file is woking ifne";
 
 $tset ="HEllo";
+$sum=0;
+$a=12;
+$b=13;
+
+function addition($num, $num2){
+
+    $sum = $num + $num2;
+
+    return $sum;
+}
 ?>
